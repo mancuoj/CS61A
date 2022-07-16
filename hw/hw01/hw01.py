941b21d294992b8978f79aeb09c4693142592a75
mancuoj <1337381768@qq.com>
2022-07-16 12:40:07 +0800

    feat: hw01

--- hw/hw01/hw01.py
+++ hw/hw01/hw01.py
@@ -14,9 +14,9 @@ def a_plus_abs_b(a, b):
     3
     """
     if b < 0:
-        f = _____
+        f = sub
     else:
-        f = _____
+        f = add
     return f(a, b)
 
 
@@ -44,7 +44,7 @@ def two_of_three(i, j, k):
     >>> two_of_three(5, 5, 5)
     50
     """
-    return _____
+    return i**2 + j**2 + k**2 - max(i, j, k)**2
 
 
 def two_of_three_syntax_check():
@@ -69,6 +69,11 @@ def largest_factor(n):
     1
     """
     "*** YOUR CODE HERE ***"
+    factor = n // 2
+    while factor:
+        if n % factor == 0:
+            return factor
+        factor -= 1
 
 
 def hailstone(n):
@@ -91,3 +96,17 @@ def hailstone(n):
     1
     """
     "*** YOUR CODE HERE ***"
+    # Pick a positive integer n as the start.
+    # If n is even, divide it by 2.
+    # If n is odd, multiply it by 3 and add 1.
+    # Continue this process until n is 1.
+    len = 1
+    while n != 1:
+        print(n)
+        len += 1
+        if n % 2 == 0:
+            n //= 2
+        else:
+            n = n*3 + 1
+    print(n)
+    return len
